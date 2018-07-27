411fcecdb3c3550a2436c380abb2570d743bf380
Harleen Thind <hathind@microsoft.com>
2018-07-26 23:05:58 -0700

    updates

--- scripts/git/release.sh
+++ scripts/git/release.sh
@@ -167,6 +167,6 @@ tag_build_publish_repo simulation-service     device-simulation-dotnet
 tag_build_publish_repo storage-service        pcs-storage-adapter-dotnet
 tag_build_publish_repo webui                  pcs-simulation-webui
 tag_build_publish_repo pcs-config-dotnet      pcs-config-dotnet
-# tag_build_publish_repo api-gateway            azure-iot-pcs-device-simulation simulation-api-gateway
+tag_build_publish_repo api-gateway            azure-iot-pcs-device-simulation simulation-api-gateway
 
 set +e
