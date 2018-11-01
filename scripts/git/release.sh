11887af653fb8fd23494408ad688586bc829a0d6
Harleen Thind <hathind@microsoft.com>
2018-10-31 22:29:18 -0700

    updates

--- scripts/git/release.sh
+++ scripts/git/release.sh
@@ -163,9 +163,9 @@ check_input
 # DOTNET Microservices
 tag_build_publish_repo simulation-service     device-simulation-dotnet
 tag_build_publish_repo pcs-diagnostics-dotnet pcs-diagnostics-dotnet
-tag_build_publish_repo storage-service        pcs-storage-adapter-dotnet
+# tag_build_publish_repo storage-service        pcs-storage-adapter-dotnet
 tag_build_publish_repo webui                  pcs-simulation-webui              device-simulation-webui
-tag_build_publish_repo pcs-config-dotnet      pcs-config-dotnet
+# tag_build_publish_repo pcs-config-dotnet      pcs-config-dotnet
 tag_build_publish_repo api-gateway            azure-iot-pcs-device-simulation   simulation-api-gateway
 
 set +e
